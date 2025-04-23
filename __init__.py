b0c59c73ca8dce4bd59f298b8d4f7c8001d94871
shrivats1995 <35287303+shrivats1995@users.noreply.github.com>
2025-04-23 12:25:02 +0530

    updating __init-_.py with relative paths for mlflow_reigstry_manager and mlflow_serve_model

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@
 
 # Expose important modules or classes for easy access
 from .device_config import cfg, mode, get_device
-from mlflow_registry_manager import get_registry_manager
-from mlflow_serve_model import get_mlflow_model_serve
+from .mlflow_registry_manager import get_registry_manager
+from .mlflow_serve_model import get_mlflow_model_serve
 from .mlflow_tracker import get_mlflow_tracker
