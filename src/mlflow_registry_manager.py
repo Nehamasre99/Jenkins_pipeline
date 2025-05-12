93f6d8a1a703ec5a4faf4974fa8bcd0f30f5359d
neha <neha.m@zentreelabs.com>
2025-05-12 14:41:15 +0530

    set tags

--- src/mlflow_registry_manager.py
+++ src/mlflow_registry_manager.py
@@ -14,7 +14,10 @@ class MLflowRegistryManager:
         model_uri = f"runs:/{run_id}/{artifact_path}"
         registered_model = mlflow.register_model(model_uri=model_uri, name=self.registry_name)
         return registered_model.version
-
+    
+    def set_model_version_tags(self, model_name: str, model_version: int, tags: dict):
+        for key, value in tags.items():
+            mlflow.set_model_version_tag(model_name, model_version, key, value)
 
 def get_registry_manager(registry_name : str):
     return MLflowRegistryManager(registry_name = registry_name)
